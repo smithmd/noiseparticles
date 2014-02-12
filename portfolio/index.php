cc0f62412ab80878e90971474a3841bfbe17670f
Michael Smith <theguitarizt@gmail.com>
2014-02-11 22:06:53 -0500

    starting to add script to load prism tag with code

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,3 +1,28 @@
-<link rel="stylesheet" type="text/css" href="/sass/prism.css" />
-<pre class="line-numbers" data-src="/portfolio/codeFiles/AcademyStudentImport.java"></pre>
+<link rel="stylesheet" type="text/css" href="/sass/prism.css"/>
+<span>AcademyStudentImport.java:
+  <a href="javascript:void(0);" onClick="loadPrism('AcademyStudentImport.java')">View</a> |
+  <a href="/portfolio/codeFiles/AcademyStudentImport.java">Download</a></span><br/>
+
+<span>CreateShortUrls.java:
+  <a href="javascript:void(0);" onclick="loadPrism('CreateShortUrls.java')">View</a> |
+  <a href="/portfolio/codeFiles/CreateShortUrls.java">Download</a></span><br />
+
+<span>GmailAddressImport.java:
+  <a href="javascript:void(0);" onclick="loadPrism('GmailAddressImport.java')">View</a> |
+  <a href="/portfolio/codeFiles/GmailAddressImport.java">Download</a></span><br />
+
+<span>ParentImport.java:
+  <a href="javascript:void(0);" onclick="loadPrism('ParentImport.java')">View</a> |
+  <a href="/portfolio/codeFiles/ParentImport.java">Download</a></span><br />
+
+<span>TravelFormReaderArrival.java:
+  <a href="javascript:void(0);" onclick="loadPrism('TravelFormReaderArrival.java')">View</a> |
+  <a href="/portfolio/codeFiles/TravelFormReaderArrival.java">Download</a></span><br />
+
+<pre id="" class="line-numbers"></pre>
 <script type="application/javascript" src="/js/prism.js"></script>
+<script type="application/javascript">
+  function loadPrism(filename) {
+
+  }
+</script>
